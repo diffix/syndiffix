99edb4412b1d63e6808043a1ab8a47704f97add0
Cristian Berneanu <bcristi@gmail.com>
2023-09-08 16:51:36 +0300

    Add tests for bucket harvesting.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 from syndiffix.common import *
+from syndiffix.counters import UniqueAidCountersFactory
+from syndiffix.forest import *
 
 SALT = bytes([])
 NOISELESS_SUPPRESSION = SuppressionParams()
@@ -9,3 +11,13 @@ NOISELESS_PARAMS.layer_noise_sd = 0.0
 NOISELESS_PARAMS.outlier_count.upper = NOISELESS_PARAMS.outlier_count.lower
 NOISELESS_PARAMS.top_count.upper = NOISELESS_PARAMS.top_count.lower
 NOISELESS_CONTEXT = AnonymizationContext(bucket_seed=np.uint64(123), anonymization_params=NOISELESS_PARAMS)
+
+
+def create_forest(
+    data_df: DataFrame, aid_df: DataFrame | None = None, anon_params: AnonymizationParams | None = None
+) -> Forest:
+    aid_df = DataFrame(data_df.index) if aid_df is None else aid_df
+    anon_params = anon_params if anon_params is not None else AnonymizationParams()
+    return Forest(
+        AnonymizationContext(Hash(0), anon_params), BucketizationParams(), UniqueAidCountersFactory(), aid_df, data_df
+    )
