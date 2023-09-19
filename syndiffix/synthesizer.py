533bffb67b64b3e276b02f173b5f1eb232fa21e1
Edon Gashi <egashi@mpi-sws.org>
2023-09-19 09:45:40 +0200

    Minor tweaks in interface

--- syndiffix/synthesizer.py
+++ syndiffix/synthesizer.py
@@ -1,4 +1,4 @@
-from typing import Callable, Optional
+from typing import Optional
 
 import pandas as pd
 
@@ -9,12 +9,7 @@ from .clustering.strategy import ClusteringStrategy, DefaultClustering
 from .common import *
 from .counters import GenericAidCountersFactory, UniqueAidCountersFactory
 from .forest import Forest
-from .microdata import (
-    DataConvertor,
-    apply_convertors,
-    generate_microdata,
-    get_convertor,
-)
+from .microdata import apply_convertors, generate_microdata, get_convertor
 
 
 def _is_integral(col_type: ColumnType) -> bool:
@@ -26,21 +21,28 @@ def _is_integral(col_type: ColumnType) -> bool:
 
 def synthesize(
     df: pd.DataFrame,
-    aid_columns: list[str] = [],
+    aid_columns: Optional[list[str]] = None,
     data_columns: Optional[list[str]] = None,
     anon_params: AnonymizationParams = AnonymizationParams(),
     bucketization_params: BucketizationParams = BucketizationParams(),
     clustering: ClusteringStrategy = DefaultClustering(),
-    convertor_factory: Callable[[pd.DataFrame, str], DataConvertor] = get_convertor,
 ) -> pd.DataFrame:
-    df_aids = df[aid_columns] if len(aid_columns) > 0 else pd.DataFrame({"RowIndex": range(1, len(df) + 1)})
+    df_raw_data = df
 
-    df_raw_data = df.drop(columns=aid_columns)
+    if aid_columns is None:
+        aid_columns = []
+
+    if len(aid_columns) > 0:
+        df_aids = df[aid_columns]
+        df_raw_data = df.drop(columns=aid_columns)
+    else:
+        df_aids = pd.DataFrame({"RowIndex": range(1, len(df) + 1)})
+
     if data_columns is not None:
         assert len(data_columns) > 0
         df_raw_data = df_raw_data[data_columns]
 
-    column_convertors = [convertor_factory(df, column) for column in df.columns]
+    column_convertors = [get_convertor(df, column) for column in df.columns]
     column_is_integral = [_is_integral(convertor.column_type()) for convertor in column_convertors]
     df_data = apply_convertors(column_convertors, df_raw_data)
 
