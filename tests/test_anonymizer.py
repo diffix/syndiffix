e2d84382e18f42d77adc21bdb1ec697668417bbb
Cristian Berneanu <bcristi@gmail.com>
2023-09-21 12:13:33 +0300

    Add tests for computing the noisy row limit.

--- tests/test_anonymizer.py
+++ tests/test_anonymizer.py
@@ -1,3 +1,5 @@
+from pytest import approx
+
 from syndiffix.anonymizer import *
 
 
@@ -5,3 +7,11 @@ def test_is_low_count() -> None:
     assert is_low_count(b"", SuppressionParams(), [(1, Hash(0))])
     assert not is_low_count(b"", SuppressionParams(), [(100, Hash(0))])
     assert is_low_count(b"", SuppressionParams(), [(100, Hash(0)), (1, Hash(0))])
+
+
+def test_noisy_row_limit() -> None:
+    assert noisy_row_limit(b"", Hash(0), 100, 1) == approx(100, abs=5)
+    assert noisy_row_limit(b"", Hash(0), 200, 2) == approx(100, abs=5)
+    assert noisy_row_limit(b"", Hash(0), 100, 10000) == 0
+    assert noisy_row_limit(b"", Hash(0), 10000, 10000) == 1
+    assert noisy_row_limit(b"", Hash(0), 1000000, 10000) == approx(100, abs=5)
