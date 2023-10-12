4a3deb8fdea55011b77f0fc3d49cec22a5d947a8
Cristian Berneanu <bcristi@gmail.com>
2023-10-12 14:49:55 +0300

    Fix refining bugs hidden by the previously fixed bug.

--- tests/test_bucket.py
+++ tests/test_bucket.py
@@ -22,28 +22,15 @@ def test_harvest_range() -> None:
     ]
 
 
-def test_refining_1() -> None:
+def test_refining() -> None:
     forest = create_forest(
-        DataFrame({"data1": [float(i) for i in range(12)], "data2": [1.0] * 12}), anon_params=NOISELESS_PARAMS
-    )
-    tree = forest.get_tree((ColumnId(0), ColumnId(1)))
-
-    assert harvest(tree) == [
-        Bucket((Interval(0.0, 8.0), Interval(1.0, 1.0)), 8),
-        Bucket((Interval(8.0, 16.0), Interval(1.0, 1.0)), 4),
-    ]
-
-
-def test_refining_2() -> None:
-    forest = create_forest(
-        DataFrame({"data1": [float(i) for i in range(12)] * 3, "data2": [1.0, 2.0, 3.0] * 12}),
+        DataFrame({"x": [float(i) for i in range(12)] * 5, "y": [1.0, 2.0, 3.0] * 20}),
         anon_params=NOISELESS_PARAMS,
     )
     tree = forest.get_tree((ColumnId(0), ColumnId(1)))
+    intervals = [bucket.intervals for bucket in harvest(tree) for _ in range(bucket.count)]
 
-    assert harvest(tree) == [
-        Bucket((Interval(0.0, 8.0), Interval(1.0, 1.0)), 9),
-        Bucket((Interval(0.0, 8.0), Interval(2.0, 4.0)), 15),
-        Bucket((Interval(8.0, 16.0), Interval(2.0, 4.0)), 9),
-        Bucket((Interval(9.0, 9.0), Interval(1.0, 1.0)), 3),
-    ]
+    for i in range(12):
+        x = float(i)
+        y = float(i % 3 + 1)
+        assert intervals.count((Interval(x, x), Interval(y, y))) == 5
